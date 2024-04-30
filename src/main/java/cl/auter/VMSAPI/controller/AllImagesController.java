8eeaa9ac10705aa7d4ff69550d4abf8a32b3e4d1
droblesauter <drobles@auter.cl>
2024-04-30 17:22:59 -0400

    update

--- src/main/java/cl/auter/VMSAPI/controller/AllImagesController.java
+++ src/main/java/cl/auter/VMSAPI/controller/AllImagesController.java
@@ -2,15 +2,10 @@ package cl.auter.VMSAPI.controller;
 
 import java.util.List;
 
-import javax.json.Json;
-import javax.json.JsonObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,17 +20,9 @@ public class AllImagesController {
 	MessageImageAllService messageImageAllService;
 	
 	@GetMapping("")
-	public List<MessageImageAllModel> allImages(@RequestBody MessageImageAllModel jsonInput) {
-		List<MessageImageAllModel> images;
-		
-		if ((jsonInput != null) && (jsonInput.getNombre_mensaje() != null) && (! jsonInput.getNombre_mensaje().equals(""))) {
-			// Filter by message name
-			String messageName = "%" + jsonInput.getNombre_mensaje().replaceAll("%", "\\\\%") + "%";
-			images = messageImageAllService.getMessageImageModelByName(messageName);
-		} else {
-			images = messageImageAllService.getMessageImageAllModel();
-		}
-		
+	public List<MessageImageAllModel> allImages() {
+		List<MessageImageAllModel> images = messageImageAllService.getMessageImageAllModel();
+				
 		return images;	
 	}
 }
